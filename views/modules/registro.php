d36600e6236246dc3c44660729551fd2ecce6e8e
steven-ospina <40034424+steven-ospina@users.noreply.github.com>
2018-06-07 13:24:05 -0500

    formulario
    mas campos del formulario

--- views/modules/registro.php
+++ views/modules/registro.php
@@ -1,9 +1,13 @@
-<div class="">
-  
+<div class="formulario">
+
 <form class="" action="index.html" method="post">
   <input type="text" name="nombre" value="" placeholder="Nombre">
   <input type="text" name="apellidos" value="" placeholder="Apellidos">
-  <input type="number" name="" value="" >
+  <input type="number" name="" value="" placeholder="Documento">
+  <input type="text" name="correo" value="" placeholder="Correo">
+  <input type="text" name="telefono" value="" placeholder="Telefono">
+  <input type="text" name="direccion" value="" placeholder="Direccion">
+  <input type="text" name="contrasena" value="" placeholder="Contraseña">
 </form>
 
 </div>
