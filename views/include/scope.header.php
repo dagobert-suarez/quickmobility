17450cb843cc27d8dadfaa11f24b0d944cdb447a
steven-ospina <40034424+steven-ospina@users.noreply.github.com>
2018-06-07 15:42:42 -0500

    header

--- views/include/scope.header.php
+++ views/include/scope.header.php
@@ -7,11 +7,17 @@
   </head>
   <body>
     <header class="header">
-      <nav>
-        <ul>
-          <li>menu 1</li>
-          <li>menu 2</li>
-          <li>menu 3</li>
-        </ul>
-      </nav>
+      <section class="container">
+        <section class="m">1</section>
+        <section class="m">2</section>
+        <section class="m">3</section>
+      </section>
     </header>
+
+  <!--  <nav class="menu">
+      <ul>
+        <li><a href="https://www.google.com">menu 1</li>
+        <li>menu 2</li>
+        <li>menu 3</li>
+      </ul>
+    </nav>-->
