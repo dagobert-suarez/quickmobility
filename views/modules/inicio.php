75bce51283d760eb8d130bdeeff6efb00e1028e8
steven-ospina <40034424+steven-ospina@users.noreply.github.com>
2018-06-07 14:54:27 -0500

    inicio

--- views/modules/inicio.php
+++ views/modules/inicio.php
@@ -1,6 +1,9 @@
-<section>
-  <div class="quines somos">
+<section class="langin_page">
+  <div class=" cuadro_1 quines_somos">
     <h1>QuickMobility</h1>
+  </div>
+  <div class="cuadro_2 Noticias">
+    <h1>Noticias Del Dia</h1>
   </div>
 </section>
 
