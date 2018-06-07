7a698f864fe6c64a2520ddff17fe35c01f8a00c2
steven-ospina <40034424+steven-ospina@users.noreply.github.com>
2018-06-07 13:19:43 -0500

    div
    crear la caja del formlario

--- views/modules/registro.php
+++ views/modules/registro.php
@@ -1,5 +1,9 @@
+<div class="">
+  
 <form class="" action="index.html" method="post">
   <input type="text" name="nombre" value="" placeholder="Nombre">
   <input type="text" name="apellidos" value="" placeholder="Apellidos">
   <input type="number" name="" value="" >
 </form>
+
+</div>
