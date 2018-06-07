160169af676de1107a6fbd7b2d1bcb0288970d98
dagobert-suarez <dagobertsuarez@gmail.com>
2018-06-07 13:04:33 -0500

    ujjjum

--- controller/views.controller.php
+++ controller/views.controller.php
@@ -12,10 +12,8 @@ class viewsController{
     require_once "views/modules/registro.php";
     require_once "views/include/scope.footer.php";
   }
-  function mas(){
-    require_once "views/include/scope.header.php";
-    require_once "views/modules/mas.php";
-    require_once "views/include/scope.footer.php";
+  function newUser(){
+    $data=$_POST['data'];
   }
 }
 ?>
