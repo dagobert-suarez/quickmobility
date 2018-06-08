db60a29f329fb7e988e377b3f4dbf011a89f660e
steven-ospina <40034424+steven-ospina@users.noreply.github.com>
2018-06-07 22:42:55 -0500

    estilo

---
+++ views/modules/login.php
@@ -0,0 +1,15 @@
+
+  <div class="formulario">
+        <h2>Iniciar Sesión</h2>
+        <form action="#" onsubmit="return validar()">
+            <input type="text" id="text1" placeholder="Usuario" onkeypress="return soloLetras(event)">
+            <div id="alertu">
+                <span id="mensau"></span>
+            </div>
+            <input type="password" id="password1" placeholder="Contraseña">
+            <div id="alertp">
+                <span id="mensap"></span>
+            </div>
+            <input type="submit" value="Iniciar Sesión" >
+        </form>
+    </div>
